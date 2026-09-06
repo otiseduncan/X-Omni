f18d491acc4ff567412c199354389cddada3eb5a
otiseduncan <otiseduncan@gmail.com>
2026-09-06 13:29:09 -0400

    Pin launcher installer to Windows PowerShell

--- Install-X-Omni-Launcher.cmd
+++ Install-X-Omni-Launcher.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\install-windows-launcher.ps1"
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\install-windows-launcher.ps1"
 if errorlevel 1 pause
 exit /b %errorlevel%
