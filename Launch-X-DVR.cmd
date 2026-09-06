0e9da67f54dd20ca2d2724cab7f70db5ea3dfb62
otiseduncan <otiseduncan@gmail.com>
2026-09-06 13:29:07 -0400

    Pin X DVR launcher to Windows PowerShell

--- Launch-X-DVR.cmd
+++ Launch-X-DVR.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-start "X DVR" /b powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0scripts\launch-x-dvr.ps1"
+start "X DVR" /b "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0scripts\launch-x-dvr.ps1"
 exit /b 0
