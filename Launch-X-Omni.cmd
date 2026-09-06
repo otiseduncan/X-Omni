cde5e78d948fa1db8ae192059d5d90b6c01af527
otiseduncan <otiseduncan@gmail.com>
2026-09-06 13:29:04 -0400

    Pin X Omni launcher to Windows PowerShell

--- Launch-X-Omni.cmd
+++ Launch-X-Omni.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-start "X Omni" /b powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0scripts\launch-x-omni.ps1"
+start "X Omni" /b "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoLogo -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0scripts\launch-x-omni.ps1"
 exit /b 0
