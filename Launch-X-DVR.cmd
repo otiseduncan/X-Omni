72c4a0a8604269612ecee6dcffa37ed745e7a157
otiseduncan <otiseduncan@gmail.com>
2026-09-06 13:27:46 -0400

    Harden X DVR command launcher

--- Launch-X-DVR.cmd
+++ Launch-X-DVR.cmd
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-start "X DVR" /b powershell.exe -NoLogo -NoProfile -WindowStyle Hidden -File "%~dp0scripts\launch-x-dvr.ps1"
+start "X DVR" /b powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp0scripts\launch-x-dvr.ps1"
 exit /b 0
